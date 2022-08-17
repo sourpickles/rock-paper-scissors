2091f63484e95bc3939df6d1e6d189ebb58ca38b
Emily <emily_nat12@icloud.com>
2022-08-17 13:08:42 -0400

    Fixed game function to play the correct number of rounds and fixed the tie arguement to prevent an undefined message

--- script.js
+++ script.js
@@ -1,46 +1,36 @@
 function getComputerChoice() {
-    var randNum = Math.floor(Math.random() * 3);
-    if (randNum == 0) {
-        computerChoice = "Rock";
-        return computerChoice;
-    } else if (randNum == 1) {
-        computerChoice = "Paper";
-        return computerChoice;
-    } else {
-        computerChoice = "Scissors";
-        return computerChoice;
-    }
+  var randNum = Math.floor(Math.random() * 3);
+  if (randNum == 0) {
+      computerChoice = "Rock";
+      return computerChoice;
+  } else if (randNum == 1) {
+      computerChoice = "Paper";
+      return computerChoice;
+  } else {
+      computerChoice = "Scissors";
+      return computerChoice;
+  }
 }
 
 function play(computerSelection, playerSelection) {
   console.log("Player choice: " + playerSelection);
   console.log("Computer choice: " + computerSelection);
-  if (playerSelection === computerSelection) {
-    console.log("It's a tie");
+  if (playerSelection == computerSelection) {
+    return "It's a tie";
   } else if (playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Paper") {
     return "Player wins";
   } else if (computerSelection == "Paper" && playerSelection == "Rock" || computerSelection == "Rock" && playerSelection == "Scissors" || computerSelection == "Scissors" && playerSelection == "Paper") {
     return "Computer wins";
-  } else {
-    return "Error";
-  }
+  } 
 }
 
 function game() {
   //plays for five rounds total. starts counting at 0
-  for (rounds = 0; rounds <= 4; rounds++) {
-    play(computerSelection, playerSelection);
-    if (rounds <= 4) {
-      game();
-      console.log(rounds);
-    } 
+  for (rounds = 0; rounds < 5; rounds++) {
+    const computerSelection = getComputerChoice();
+    let playerSelection = prompt("Rock, Paper, or Scissors: ");
+    console.log(play(playerSelection, computerSelection));
     }
-  }
-
-var rounds = 0;
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("Rock, Paper, or Scissors: ");
+}
 
 game()
-
-//play the correct number of games and show prompt each round
