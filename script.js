36980649bb4dff6411f827a63fa4854d6c84d957
Emily <emily_nat12@icloud.com>
2022-08-17 00:04:39 -0400

    Created game function to control the number of rounds the user can play - unfinished

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
 function getComputerChoice() {
     var randNum = Math.floor(Math.random() * 3);
     if (randNum == 0) {
-        computerSelection = "Rock";
-        return computerSelection;
+        computerChoice = "Rock";
+        return computerChoice;
     } else if (randNum == 1) {
-        computerSelection = "Paper";
-        return computerSelection;
+        computerChoice = "Paper";
+        return computerChoice;
     } else {
-        computerSelection = "Scissors";
-        return computerSelection;
+        computerChoice = "Scissors";
+        return computerChoice;
     }
-}
-
-function getPlayerChoice() {
-    var playerSelection = prompt("Enter your choice: ");
-    return playerSelection
 }
 
 function play(computerSelection, playerSelection) {
-    console.log(getPlayerChoice());
-    console.log(getComputerChoice());
-    if (computerSelection === playerSelection) {
-      console.log("Its a tie");
-    } else {
-      console.log("Its not a tie");
-    }
+  console.log("Player choice: " + playerSelection);
+  console.log("Computer choice: " + computerSelection);
+  if (playerSelection === computerSelection) {
+    console.log("It's a tie");
+  } else if (playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Paper") {
+    return "Player wins";
+  } else if (computerSelection == "Paper" && playerSelection == "Rock" || computerSelection == "Rock" && playerSelection == "Scissors" || computerSelection == "Scissors" && playerSelection == "Paper") {
+    return "Computer wins";
+  } else {
+    return "Error";
+  }
 }
 
-//issue: console always shows "Its a tie"
+function game() {
+  //plays for five rounds total. starts counting at 0
+  for (rounds = 0; rounds <= 4; rounds++) {
+    play(computerSelection, playerSelection);
+    if (rounds <= 4) {
+      game();
+      console.log(rounds);
+    } 
+    }
+  }
+
+var rounds = 0;
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Rock, Paper, or Scissors: ");
+
+game()
+
+//play the correct number of games and show prompt each round
