983cd4a8e1d80273fa26b73b36c8c0b2b82a2a32
Emily <emily_nat12@icloud.com>
2022-08-18 17:23:01 -0400

    Game reworked to improve functionality and address bugs

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-function getComputerChoice() {
+function getComputerChoice () {
   var randNum = Math.floor(Math.random() * 3);
   if (randNum == 0) {
-      computerChoice = "rock";
-      return computerChoice;
+    return "rock";
   } else if (randNum == 1) {
-      computerChoice = "paper";
-      return computerChoice;
+    return "paper";
   } else {
-      computerChoice = "scissors";
-      return computerChoice;
+    return "scissors"
   }
 }
 
 function play(computerSelection, playerSelection) {
-  // Swiched because the console.log was printing the wrong varaiable
-  console.log ("Computer choice: " + playerSelection);
-  console.log("Player choice: " + computerSelection);
-  if (playerSelection == computerSelection) {
-    return "It's a tie";
-  } else if (playerSelection == "paper" && computerSelection == "rock" || playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper") {
-    return "Player wins";
-  } else if (computerSelection == "paper" && playerSelection == "rock" || computerSelection == "rock" && playerSelection == "scissors" || computerSelection == "scissors" && playerSelection == "paper") {
-    return "Computer wins";
-  } 
+  if (playerSelection === computerSelection) {
+    return "Its a tie!";
+  } else if (playerSelection == "rock" && computerSelection == "scissors" ||
+      playerSelection == "scissors" && computerSelection == "paper" ||
+      playerSelection == "paper" && computerSelection == "rock") {
+    return "Player wins!";
+  } else if (computerSelection == "rock" && playerSelection == "scissors" ||
+      computerSelection == "scissors" && playerSelection == "paper" ||
+      computerSelection == "paper" && playerSelection == "rock") {
+    return "Computer wins!";
+  }
 }
 
 function game() {
+  var playerSelection = "";
+  var computerSelection = "";
   //plays for five rounds total. starts counting at 0
   for (rounds = 0; rounds < 5; rounds++) {
-    const computerSelection = getComputerChoice();
-    const playerSelection = prompt("Rock, Paper, or Scissors: ").toLowerCase();
+    play(playerSelection, computerSelection);
+    var playerSelection = prompt("Rock, Paper, or Scissors: ").toLowerCase();
+    var computerSelection = getComputerChoice();
+    console.log("Player picks " + playerSelection);
+    console.log("Computer picks " + computerSelection);
     console.log(play(playerSelection, computerSelection));
   }
 }
